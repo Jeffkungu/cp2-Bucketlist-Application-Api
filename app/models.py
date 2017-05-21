90c313246efe291ce895f38cd4674b684b513404
Jeffkungu <Jeffkungu@gmail.com>
2017-05-21 17:33:31 +0300

    Adds functions to user model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,41 @@
 # import db connection
 from app import db
 import datetime
+from flask_bcrypt import Bcrypt
 
 
+class User(db.Model):
+    '''Create User Table'''
+
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(250), unique=True)
+    password = db.Column(db.String(250))
+    bucketlist = db.relationship('Bucketlist', order_by='Bucketlist.id',
+                                  cascade='all, delete-orphan')
+
+    def __init__(self, email, password):
+        """Initialize user with email and password."""
+
+        self.email = email
+        self.password = Bcrypt().generate_password_hash(password).decode()
+
+    def validate_password(self, password):
+        """
+        Checks the password matches the hashed password saved in db
+        """
+
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def save(self):
+        """
+        Saves a new user added to the data base
+        Saves updates made on user existing in the data base
+        """
+        db.session.add(self)
+        db.session.commit()
 
 class Bucketlist(db.Model):
     """Create bucketlist table."""
@@ -15,13 +48,18 @@ class Bucketlist(db.Model):
     date_modified = db.Column(
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
+    created_by = db.Column(db.Integer, db.ForeignKey(User.id))    
 
-    def __init__(self, name):
-        """initialize with name."""
+    def __init__(self, name, created_by):
+        """initialize with name, and user who created the bucketlist."""
         self.name = name
+        self.created_by = created_by
 
     def save(self):
-        '''Adds a new bucketlist to db'''
+        '''
+        Adds a new bucketlist to the data base
+        Updatees an existing bucketlist
+        '''
         db.session.add(self)
         db.session.commit()
 
@@ -33,8 +71,8 @@ class Bucketlist(db.Model):
 
     @staticmethod
     def get_all():
-        '''Gets all bucketlists in a single query'''
-        return Bucketlist.query.all()
+        '''Gets all bucketlists created by a specific user in a single query'''
+        return Bucketlist.query.filter_by(created_by=user_id)
 
 
     def __repr__(self):
@@ -70,14 +108,4 @@ class BucketListItem(db.Model):
         self.bucketlist_id = bucketlist_id                          
 
 
-class User(db.Model):
-    '''Create User Table'''
 
-    __tablename__ = 'user'
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(50))
-    bucketlist = db.relationship('Bucketlist', backref='user',
-                                 cascade='all,delete', passive_deletes=True)       
